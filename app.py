1cb3d13941429990186ba4a6099bb4e9ee698f25
Sergey Kalistratov <kalistratov.sergey@feg.eu>
2020-11-26 13:57:52 +0100

    some mock data added

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/getTags')
+def getTags():
+    return {"tags":["smazak","buger","kridla","polevka","vegan"]}
+
+@app.route('/getTopOffers')
+def getTopOffers():
+    return {"offers":[{"name":"text1", "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"name":"text1", "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"name":"text1", "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}]}
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
