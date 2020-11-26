97d2e041323306d5488fade02ffa5f2efa91b84b
Sergey Kalistratov <kalistratov.sergey@feg.eu>
2020-11-26 13:16:53 +0100

    requirements added

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
