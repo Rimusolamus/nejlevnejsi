3849c318eafb671317cc4e3230ed6a359659a192
root <you@example.com>
2020-12-08 01:59:49 +0100

    covered with wsgi

--- nejlevnejsi.py
+++ nejlevnejsi.py
@@ -5,10 +5,14 @@ app = Flask(__name__)
 def getTags():
     return {"tags":["smazak","burger","kridla","polevka","vegan"]}
 
+@app.route('/')
+def index():
+    return "it works"
+
 @app.route('/getTopOffers')
 def getTopOffers():
     return {"offers":[{"name":"text1", "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"name":"text2", "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"name":"text3", "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}]}
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=80, host='0.0.0.0')
+    app.run(threaded=True, host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from nejlevnejsi import app
+
+if __name__ == "__main__":
+    app.run()
